b1299acfb9107b46b9d63a13bad2c983efa647b4
SoumyaOk4-4 <94857677+SoumyaOk4-4@users.noreply.github.com>
2022-06-23 08:21:23 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+class words {
+	protected String z;
+	public void hello(String x, String y) {
+		z = x + " " + y + "!";
+		System.out.println(z);
+	}
+}
+public class Main extends words {
+	public static void main(String[] args) {
+		String a = "Hello";
+		String b = "World";
+		Main probj = new Main();
+		probj.hello(a, b);
+	}
+} //Hello World!
